25ec15155e8ecff9e31159a1db56cc2157cebc92
hussainbinfazal <mdhussain0610@gmail.com>
2026-06-01 02:32:03 +0530

    HTML File added at the root level

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,42 +8,42 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     
-    {
-      url:`${process.env.NEXT_PUBLIC_BASE_URL}/#home`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url:`${process.env.NEXT_PUBLIC_BASE_URL}/#about`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url:`${process.env.NEXT_PUBLIC_BASE_URL}/#services`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url:`${process.env.NEXT_PUBLIC_BASE_URL}/#pricing`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url:`${process.env.NEXT_PUBLIC_BASE_URL}/#contact`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url:`${process.env.NEXT_PUBLIC_BASE_URL}/#trainers`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
+    // {
+    //   url:`${process.env.NEXT_PUBLIC_BASE_URL}/#home`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 1,
+    // },
+    // {
+    //   url:`${process.env.NEXT_PUBLIC_BASE_URL}/#about`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 1,
+    // },
+    // {
+    //   url:`${process.env.NEXT_PUBLIC_BASE_URL}/#services`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 1,
+    // },
+    // {
+    //   url:`${process.env.NEXT_PUBLIC_BASE_URL}/#pricing`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 1,
+    // },
+    // {
+    //   url:`${process.env.NEXT_PUBLIC_BASE_URL}/#contact`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 1,
+    // },
+    // {
+    //   url:`${process.env.NEXT_PUBLIC_BASE_URL}/#trainers`,
+    //   lastModified: new Date(),
+    //   changeFrequency: "weekly",
+    //   priority: 1,
+    // },
     ]
 
   return [
